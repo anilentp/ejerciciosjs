c874c72957a52aede738c1811117d63df01c8527
anilen <anilen.tp@gmail.com>
2016-06-17 08:00:33 -0400

    ejercicios js

---
+++ estructuras repetitivas/js/ejercicio.js
@@ -0,0 +1,11 @@
+
+// Ejercicio Triangulo
+for (var i = 1; 1<=22; i++){
+	for(var j = 1; 1<=22; j++){
+		for(var k = 1; 1<=22; k++){
+			if ((i*i)+(j*j)===(k*k)){
+				console.log ("(" + i + "," + j + "," + k + ")");
+			}
+		}
+	}
+}
